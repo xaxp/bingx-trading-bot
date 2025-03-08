afb983fd547fa04a3d6f84520859a2fc51c0136e
xaxp <tomas.peterka@gmail.com>
2025-03-08 06:52:38 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('BingX Trading Bot is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server běží na portu ${PORT}`);
+});
